595bbdbe45aefce0e8977119eab8a7a12381d7ff
gloriarobas <gloria.robas@stud.ubbcluj.ro>
2022-01-05 17:30:45 +0200

    p

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from Logic.crud import adauga_cheltuiala
-from Tests.run_all import run_all_tests
-from UI.console import run_menu
-
-def main():
-    lista = []
-    run_menu(lista)
-
-run_all_tests()
-main()
+p
